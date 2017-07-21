357dfbe9aa88b21125e7113b34e1e46fcd7e7d79
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-22 02:53:53 +0300

    Test IG Api

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,3 @@
-fetch('https://www.instagram.com/evgeni.tsn/?__a=1')
+fetch('https://api.instagram.com/v1/users/37619101/media/recent?access_token=37619101.34e42b0.225c784d9f6948b7929faf8835681018')
 .then((data) => data.json())
 .then((data) => console.log(data))
