026b68881300bf7b8dcab2ca9d5e0e5a299a194e
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-22 03:10:40 +0300

    Implemented IG access from the site

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,14 @@
-fetch('https://api.instagram.com/v1/users/37619101/media/recent?access_token=37619101.34e42b0.225c784d9f6948b7929faf8835681018')
-.then((data) => data.json())
-.then((data) => console.log(data))
+var url = 'https://api.instagram.com/v1/users/37619101/media/recent?access_token=37619101.34e42b0.225c784d9f6948b7929faf8835681018'
+
+$.ajax({
+    url: url,
+    type: "GET",
+    crossDomain: true,
+    dataType: "jsonp",
+    success: function(data){
+        var imageArr = data.data;
+        for(var image of imageArr){
+            console.log(image.caption.text)
+        }
+    }
+});
