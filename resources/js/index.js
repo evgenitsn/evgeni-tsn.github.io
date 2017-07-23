481313a94469e2c6d69309f87ebfe7c08ca27dcc
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-23 23:04:27 +0300

    Added old browsers support libraries and started implementing sticky header

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,32 +1,34 @@
 var url = 'https://api.instagram.com/v1/users/37619101/media/recent?access_token=37619101.34e42b0.225c784d9f6948b7929faf8835681018'
 
-$.ajax({
-    url: url,
-    type: "GET",
-    crossDomain: true,
-    dataType: "jsonp",
-    success: function(data){
-        var imageArr = data.data;
-        for(var image of imageArr){
-            console.log(image.caption.text)
-        }
-    }
-});
-
-
-$('#submitbtn').click(function(e) {
-    e.preventDefault()
-    var name = $('#name').val()
-    var email = $('#email').val()
-    var message = $('#message').val()
+$(document).ready(function() {
     $.ajax({
-        url: "https://formspree.io/evgeni.tsn@gmail.com", 
-        method: "POST",
-        data: {
-            name: name, 
-            email: email, 
-            message: message
-        },
-        dataType: "json"
+        url: url,
+        type: "GET",
+        crossDomain: true,
+        dataType: "jsonp",
+        success: function(data){
+            var imageArr = data.data;
+            for(var image of imageArr){
+                console.log(image.caption.text)
+            }
+        }
     });
+
+
+    $('#submitbtn').click(function(e) {
+        e.preventDefault()
+        var name = $('#name').val()
+        var email = $('#email').val()
+        var message = $('#message').val()
+        $.ajax({
+            url: "https://formspree.io/evgeni.tsn@gmail.com", 
+            method: "POST",
+            data: {
+                name: name, 
+                email: email, 
+                message: message
+            },
+            dataType: "json"
+        });
+    })
 })
