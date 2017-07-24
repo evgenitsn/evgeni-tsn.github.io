378e44a99daa6baef204494677f268c7e6b2a759
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-25 02:02:51 +0300

    Build up links and scrolling to the sections via the menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -41,4 +41,39 @@ $(document).ready(function() {
     }, {
         offset: '15%'
     })
+
+    $('.js-scroll-to-contact').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js-section-contact').offset().top
+        }, 1000)
+    })
+
+    $('.js-scroll-to-start').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js-section-start').offset().top
+        }, 1000)
+    })
+
+    // Navigation scroll
+
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(event) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+                }, 1000, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { // Checking if the target was focused
+                    return false;
+                } else {
+                    $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                    $target.focus(); // Set focus again
+                };
+            });
+        }}
+    });
 })
