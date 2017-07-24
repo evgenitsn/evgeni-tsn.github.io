50fe2f79cf243dfab88989588a2f48b773eeb80b
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-25 02:25:42 +0300

    Added some showing animations

--- resources/js/index.js
+++ resources/js/index.js
@@ -76,4 +76,28 @@ $(document).ready(function() {
             });
         }}
     });
+
+    $('.js-wp-1').waypoint(function(direction) {
+        $('.js-wp-1').addClass('animated fadeIn')
+    }, {
+        offset: '75%'
+    })
+
+    $('.js-wp-2').waypoint(function(direction) {
+        $('.js-wp-2').addClass('animated fadeIn')
+    }, {
+        offset: '75%'
+    })
+
+    $('.js-wp-3').waypoint(function(direction) {
+        $('.js-wp-3').addClass('animated fadeIn')
+    }, {
+        offset: '75%'
+    })
+
+    $('.js-wp-4').waypoint(function(direction) {
+        $('.js-wp-4').addClass('animated fadeIn')
+    }, {
+        offset: '75%'
+    })
 })
