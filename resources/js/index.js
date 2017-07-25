f8a96bb3bdaf0aba60448c464919cdb4bff0370b
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-07-26 00:00:24 +0300

    Added responsive menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -100,4 +100,19 @@ $(document).ready(function() {
     }, {
         offset: '75%'
     })
+
+    // Mobile Nav
+
+    var nav = $('.js-main-nav')
+    var icon = $('.js-nav-icon i')
+    $('.js-main-nav li a, .js-nav-icon').click(function() {
+        nav.slideToggle(200)
+        if(icon.hasClass('fa-bars')){
+            icon.addClass('fa-times')
+            icon.removeClass('fa-bars')
+        } else {
+            icon.addClass('fa-bars')
+            icon.removeClass('fa-times')
+        }
+    })
 })
