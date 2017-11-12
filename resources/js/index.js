6ed9798fe6c83efc04c35cb5698cb39d52c4cf49
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-11-12 21:48:00 +0200

    Compressed images / Minified CSS

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,18 +1,18 @@
 var url = 'https://api.instagram.com/v1/users/37619101/media/recent?access_token=37619101.34e42b0.225c784d9f6948b7929faf8835681018'
 
 $(document).ready(function() {
-    $.ajax({
-        url: url,
-        type: "GET",
-        crossDomain: true,
-        dataType: "jsonp",
-        success: function(data){
-            var imageArr = data.data;
-            for(var image of imageArr){
-                console.log(image.caption.text)
-            }
-        }
-    });
+    // $.ajax({
+    //     url: url,
+    //     type: "GET",
+    //     crossDomain: true,
+    //     dataType: "jsonp",
+    //     success: function(data){
+    //         var imageArr = data.data;
+    //         for(var image of imageArr){
+    //             console.log(image.caption.text)
+    //         }
+    //     }
+    // });
 
 
     $('#submitbtn').click(function(e) {
