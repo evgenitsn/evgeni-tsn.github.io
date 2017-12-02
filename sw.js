ad7301eecd57478801c61a0c9dda084b5726e2b4
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-12-02 23:05:13 +0200

    Change in Service Worker caching

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v2';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var CACHE_STATIC_NAME = 'static-v3';
+var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 var STATIC_FILES = [
   '/',
   '/index.html',
@@ -64,6 +64,17 @@ var STATIC_FILES = [
   '/vendors/js/selectivizr.min.js',
 ];
 
+self.addEventListener('install', function(event) {
+  // console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME)
+      .then(function(cache) {
+        // console.log('[Service Worker] Precaching App Shell');
+        cache.addAll(STATIC_FILES);
+      })
+  )
+});
+
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys()
@@ -89,7 +100,7 @@ self.addEventListener('fetch', function(event) {
             .then(function(res) {
               return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
+                  // cache.put(event.request.url, res.clone());
                   return res;
                 })
             })
